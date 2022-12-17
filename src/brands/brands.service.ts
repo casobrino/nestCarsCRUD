80777ffde08ca74aba62f0adb4d0eab0a2d84d4d
Cesar Sobrino <casobrino_0@hotmail.com>
2022-12-16 19:41:13 -0600

    Brands added with nest generator reource, a new CRUD on the backend

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { v4 as uuid } from 'uuid';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLowerCase(),
+      createdAt: new Date().getTime(),
+    };
+
+    this.brands.push(brand);
+    return brand;
+  }
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand with ${id} not found`);
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        brand.updatedAt = new Date().getTime();
+        brandDB = { ...brandDB, ...updateBrandDto };
+        return brandDB;
+      }
+      return brand;
+    });
+    return brandDB;
+  }
+
+  remove(id: string) {
+    this.brands = this.brands.filter((brand) => brand.id !== id);
+  }
+}
